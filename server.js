0fd6921693f9260d30828deed1a3c5549612fd7a
Eva Raymond <evathecoderr@outlook.com>
2017-10-15 18:37:07 +0100

    simple server validation, api virtual route

--- server.js
+++ server.js
@@ -1,10 +1,47 @@
 const jsonServer = require('json-server');
+//Create express server
 const server = jsonServer.create();
+//JSON server router
 const router = jsonServer.router('db.json');
+//Use default options for middlewares
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3002;
+const port = process.env.PORT || 3000;
 
+// Set default middlewares (logger, static, cors and no-cache)
 server.use(middlewares);
-server.use(router);
+// To handle POST, PUT and PATCH you need to use a body-parser
+// You can use the one used by JSON Server
+server.use(jsonServer.bodyParser)
+server.use((req, res, next) => {
+    if (req.method === 'POST' || req.method === 'PUT') {
+        let title = req.body.title
+        let categories = req.body.categories
+        let content = req.body.content
+        if(title && categories && content && (
+            title !== '' && categories !== '' && content !== ''
+        )){
+            req.body.timeStamp = Date.now()
+        }
+        else {
+            return res.status(400).end('Missing a snap field')
+        }
 
+    }
+    // Continue to JSON Server router
+    next()
+})
+
+router.render = (req, res) => {
+    if(Object.keys(res.locals.data).length == 0){
+        res.jsonp({
+          error: "404 error not found"
+        })
+    }
+    else res.jsonp(res.locals.data)
+  }
+
+//Use default router - in-memory database
+// server.use(router);
+server.use('/api', router)
+//start server
 server.listen(port);
